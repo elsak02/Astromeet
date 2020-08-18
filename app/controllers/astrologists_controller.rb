32838158e96fa250e73cb0cce06ef83de75428f6
Oceane Leroy oceaneleroy1@gmail.com <oceaneleroy1@gmail.com>
2020-08-18 14:32:40 +0200

    created the astrologist show & updated footer

--- app/controllers/astrologists_controller.rb
+++ app/controllers/astrologists_controller.rb
@@ -2,4 +2,8 @@ class AstrologistsController < ApplicationController
   def index
     @astrologists = Astrologist.all
   end
+
+  def show
+    @astrologist = Astrologist.find(params[:id])
+  end
 end
