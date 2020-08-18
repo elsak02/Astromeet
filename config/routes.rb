bae8cdd55d1d630467494ce6aeeb30c8205a0e48
Elsa Kiefer <elsa.kiefer@edhec.com>
2020-08-18 15:15:44 +0200

    edit/update astrologist

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :astrologists, only: [:index, :show]
-  resources :users, only: [:show] do
-    resources :astrologists, only: [:new, :create]
-  end
+  resources :astrologists, only: [:index, :show, :new, :create, :edit, :update]
+  resources :users, only: [:show]
 end
+
+
