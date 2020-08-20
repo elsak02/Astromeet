a7c677847fa16f1d95fa860ae07e79bd6813311b
Elsa Kiefer <elsa.kiefer@edhec.com>
2020-08-20 15:35:56 +0200

    reviews-without-design

--- app/javascript/components/review_form.js
+++ app/javascript/components/review_form.js
@@ -1,17 +1,19 @@
-
-
 const reviewForm = () => {
-  console.log("hello")
-  const reviewButton = document.querySelector("#review-button");
-  const cancelButton = document.querySelector("#cancel-button");
-  const collapseForm = document.querySelector("#collapseExample")
-  if (reviewButton) {
-    reviewButton.addEventListener( "click", () => {
-      reviewButton.classList.toggle("d-none")
-    });
-  cancelButton.addEventListener ("click", () => {
-    reviewButton.click();
-  })
+  const reviewButtons = document.querySelectorAll(".review-button");
+  const cancelButtons = document.querySelectorAll("#cancel-button");
+  if (reviewButtons.length > 0) {
+    reviewButtons.forEach(reviewButton => {
+      reviewButton.addEventListener( "click", () => {
+        reviewButton.classList.toggle("d-none")
+      });
+    })
+    cancelButtons.forEach(cancelButton => {
+      cancelButton.addEventListener ("click", () => {
+        const bookingId = cancelButton.dataset.bookingId;
+        const reviewButton = document.querySelector(`#review-button${bookingId}`)
+        reviewButton.click();
+      })
+    })
   }
 }
 
