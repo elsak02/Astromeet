edde66d378d953da94317b02d27e3214c8ac74b8
Elsa Kiefer <elsa.kiefer@edhec.com>
2020-08-21 15:28:12 +0200

    last modif

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -4,15 +4,18 @@ import flatpickr from "flatpickr";
 
 const initFlatpicker = () => {
   const dateElement = document.querySelector('#booking_date');
-  const dates = JSON.parse(dateElement.dataset.unavailableDates);
+  if (dateElement) {
+    const dates = JSON.parse(dateElement.dataset.unavailableDates);
 
 
-   flatpickr(".datepicker", {
-    minDate: "today",
-    disable: dates,
-    altInput: true,
-    inline: true,
-  });
+    flatpickr(".datepicker", {
+      minDate: "today",
+      disable: dates,
+      altInput: true,
+      inline: true,
+    });
+
+  }
 }
 
 export { initFlatpicker }
