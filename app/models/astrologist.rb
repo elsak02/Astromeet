4a0fc28785c8e2e6952cab2fa65e0c9890bd44fa
Elsa Kiefer <elsa.kiefer@edhec.com>
2020-08-21 12:35:26 +0200

    search and nav bar optimization

--- app/models/astrologist.rb
+++ app/models/astrologist.rb
@@ -8,11 +8,20 @@ class Astrologist < ApplicationRecord
   validates :price, presence: true
   validates :speciality, presence: true
 
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [:speciality],
+    associated_against: {
+      user: [:first_name, :last_name]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
+
   def rating_average
     return 0 if reviews.empty?
 
     ratings = reviews.pluck(:rating)
     return (ratings.sum / ratings.length)
   end
-
 end
